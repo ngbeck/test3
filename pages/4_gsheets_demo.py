9246673bcd0b2b83f4583f5a9a954dc11f7921d8
Nathan Beck <Nathan.beck47@gmail.com>
2023-12-06 05:23:57 +0000

    gsheets

--- pages/4_gsheets_demo.py
+++ pages/4_gsheets_demo.py
@@ -3,7 +3,7 @@ from streamlit_gsheets import GSheetsConnection
 
 url = "https://docs.google.com/spreadsheets/d/1lwRc-ftgk00ZcN-ARh2rpm0fX8x991aAwwNa-qvUddI/edit?usp=sharing"
 
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+conn = st.connection("gsheets", type=GSheetsConnection)
 
 data = conn.read(spreadsheet=url, usecols=[0, 1])
 st.dataframe(data)
