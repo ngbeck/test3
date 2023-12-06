0aefad8e739c1215228e0a1cb391d89bb005242f
Nathan Beck <Nathan.beck47@gmail.com>
2023-12-06 05:55:05 +0000

    geawg

--- pages/4_gsheets_demo.py
+++ pages/4_gsheets_demo.py
@@ -5,11 +5,6 @@ url = "https://docs.google.com/spreadsheets/d/1lwRc-ftgk00ZcN-ARh2rpm0fX8x991aAw
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(
-    worksheet="Oilers Influence 18-19",
-    ttl="10m",
-    nrows=3,
-)
-
-for row in df.intertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+#add nrows to manage rows and usecols to manage columns
+data = conn.read(spreadsheet=url)
+st.dataframe(data)
