4338f89cca191ec9694500eee89eafac4018d1b6
Nathan Beck <Nathan.beck47@gmail.com>
2023-12-06 05:14:01 +0000

    gae

--- pages/4_gsheets_demo.py
+++ pages/4_gsheets_demo.py
@@ -1,1 +1,9 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1lwRc-ftgk00ZcN-ARh2rpm0fX8x991aAwwNa-qvUddI/edit?usp=sharing"
+
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(spreadsheet=url, usecols=[0, 1])
+st.dataframe(data)
